9e24f0ab97b2a75581658a1bc4c78a1ad0a1fda6
Guenther Brunthaler <gb@xrpi3.emgenxx69lwyn5ctlr4nl64ul.local>
2017-08-03 23:22:57 +0200

    Implement Knuth's additive random number generator
    Also ensure that the results will not be affected by the endianness of
    the local platform.

--- dependencies.mk
+++ dependencies.mk
@@ -1,17 +1,19 @@
 append_rest_of_file.o: append_rest_of_file.c \
- append_rest_of_file-6p7cqka4jp2g26pkn2f0pyleu.h
-prng_3xa.o: prng_3xa.c rand-uhrltlhmrn3cke0bsezw006ys.h \
- ruser-qv7m1cmnh6dr7cl740wg6ss3w.h
-prng_isaac.o: prng_isaac.c rand-uhrltlhmrn3cke0bsezw006ys.h \
- ruser-qv7m1cmnh6dr7cl740wg6ss3w.h rand.h standard.h
-prng_u32.o: prng_u32.c rand-uhrltlhmrn3cke0bsezw006ys.h \
- ruser-qv7m1cmnh6dr7cl740wg6ss3w.h
+ include/append_rest_of_file-6p7cqka4jp2g26pkn2f0pyleu.h
+prng_3xa.o: prng_3xa.c include/dim-sdbrke8ae851uitgzm4nv3ea2.h \
+ include/rand-uhrltlhmrn3cke0bsezw006ys.h \
+ include/ruser-qv7m1cmnh6dr7cl740wg6ss3w.h
+prng_isaac.o: prng_isaac.c include/dim-sdbrke8ae851uitgzm4nv3ea2.h \
+ include/rand-uhrltlhmrn3cke0bsezw006ys.h \
+ include/ruser-qv7m1cmnh6dr7cl740wg6ss3w.h rand.h standard.h
+prng_u32.o: prng_u32.c include/rand-uhrltlhmrn3cke0bsezw006ys.h \
+ include/ruser-qv7m1cmnh6dr7cl740wg6ss3w.h
 rand.o: rand.c standard.h rand.h
-with_FILE.o: with_FILE.c with_FILE-ctm26us35u661x9z4pyf8z0e1.h \
- ruser-qv7m1cmnh6dr7cl740wg6ss3w.h
+with_FILE.o: with_FILE.c include/with_FILE-ctm26us35u661x9z4pyf8z0e1.h \
+ include/ruser-qv7m1cmnh6dr7cl740wg6ss3w.h
 with_fflush_all.o: with_fflush_all.c \
- with_fflush_all-0p13ursnxj34l8jy5bmjp4m70.h \
- ruser-qv7m1cmnh6dr7cl740wg6ss3w.h
+ include/with_fflush_all-0p13ursnxj34l8jy5bmjp4m70.h \
+ include/ruser-qv7m1cmnh6dr7cl740wg6ss3w.h
 with_malloc_buffer.o: with_malloc_buffer.c \
- with_malloc_buffer-9j76cc1vs6bi14ip2ii9f1yla.h \
- ruser-qv7m1cmnh6dr7cl740wg6ss3w.h
+ include/with_malloc_buffer-9j76cc1vs6bi14ip2ii9f1yla.h \
+ include/ruser-qv7m1cmnh6dr7cl740wg6ss3w.h
--- prng_3xa.c
+++ prng_3xa.c
@@ -1,4 +1,89 @@
+#include <dim-sdbrke8ae851uitgzm4nv3ea2.h>
 #include <rand-uhrltlhmrn3cke0bsezw006ys.h>
+#include <assert.h>
+#include <math.h>
+
+typedef u32 (*arng)[55];
+
+struct prng_state {
+   arng k1, k2, k3;
+   unsigned i;
+};
+
+static u32 gen_k1(arng a, unsigned n) {
+   /* Additive Random Number Generator:
+    *
+    * rnd[n]:= (rnd[n - 24] + rnd[n - 55]) mod m
+    *
+    * where
+    * n >= 55
+    * m is even
+    * rnd[0 .. 54] are arbitrary seed integers, not all of them even
+    *
+    * The generated sequence will have a period of at least 2 ^ 55 - 1. */
+   unsigned const m_minus_1= 0xffffffff;
+   unsigned n_minus_24;
+   assert(DIM(*a) == 55);
+   assert(n < 55);
+   if ((n_minus_24= n + (55 - 24)) >= 55) n_minus_24-= 55;
+   assert(n_minus_24 < 55);
+   return (*a)[n]+= (*a)[n_minus_24] & m_minus_1;
+}
+
+static u32 gen_k3(struct prng_state *state) {
+   unsigned i;
+   u32 r= gen_k1(state->k1, i= state->i);
+   r^= gen_k1(state->k2, i);
+   r^= gen_k1(state->k3, i);
+   if (++i >= 55) i= 0;
+   state->i= i;
+   return r;
+}
+
+static double generate_next(void *random_context) {
+   double r;
+   struct prng_state *state= random_context;
+   r= ldexp(gen_k3(state), -32);
+   return ldexp(r + gen_k3(state), -32);
+}
+
+static void seed_k(arng a, random_u32 rng, void *random_context) {
+   u32 or= 0;
+   unsigned n;
+   for (n= DIM(*a); n--; ) or|= (*a)[n]= (*rng)(random_context);
+   (*a)[0]|= ~or & 1; /* Make first element odd if all elements are even. */
+}
+
+struct preseed {
+   random_u32 rng;
+   void *random_context;
+   struct prng_state *state;
+};
+
+static void preseed_k1(struct preseed *p, arng a1) {
+   unsigned i;
+   for (i= DIM(*a1); i--; ) {
+      unsigned j;
+      u32 r= 0;
+      for (j= 4; j--; ) r= r << 8 | (*p->rng)(p->random_context) >> 32 - 8;
+      (*a1)[i]= r;
+   }
+}
+static char const *preseed_helper_cont(void *context) {
+   struct preseed *pre= context;
+   preseed_k1(pre, pre->state->k1);
+   preseed_k1(pre, pre->state->k2);
+   preseed_k1(pre, pre->state->k3);
+   return 0;
+}
+
+static char const *preseed_helper(unsigned seed, struct prng_state *rctx) {
+   struct preseed pre;
+   pre.state= rctx;
+   return prng_u32(
+      seed, &pre.rng, &pre.random_context, preseed_helper_cont, &pre
+   );
+}
 
 char const *prng_3xa(
       unsigned seed /* Random seed. */
@@ -6,5 +91,10 @@ char const *prng_3xa(
    ,  void **rgen_context /* Where to put its context. */
    ,  resource_user body, void *context
 ) {
-   return 0;
+   struct prng_state rctx;
+   char const *error;
+   if (error= preseed_helper(seed, &rctx)) return error;
+   assert(rgen); *rgen= &generate_next;
+   assert(rgen_context); *rgen_context= &rctx;
+   return (*body)(context);
 }
