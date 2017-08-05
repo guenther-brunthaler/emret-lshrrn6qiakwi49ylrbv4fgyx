d83f0bd6af2bcf03b684f7c1204f692263e026fb
Guenther Brunthaler <gb@xrpi3.emgenxx69lwyn5ctlr4nl64ul.local>
2017-08-05 04:08:47 +0200

    Make buffer grow exponentially
    But limit the maximum size increment to a sensible I/O size as a
    compromise between overstated buffer size estimation and the actual time
    to read the data.

--- append_rest_of_file.c
+++ append_rest_of_file.c
@@ -4,20 +4,28 @@
 #include <assert.h>
 
 char const *append_rest_of_file(void *context) {
+   size_t const initial_bufsz_inc= 8000;
+   size_t const max_bufsz_inc= 8000000;
    struct append_rest_of_file_vars *v= context;
    char *buffer= *(void **)v->buffer_ref;
-   size_t allocated= *v->size_ref, used= 0;
+   size_t allocated, used;
    FILE *fh= v->fh;
+   size_t bufsz_inc= initial_bufsz_inc;
+   allocated= used= *v->size_ref;
    for (;;) {
       size_t read;
       if (allocated == used) {
          size_t new_size;
-         size_t const bufsz_inc= 8000;
          void *new_buf= realloc(
             buffer, new_size= allocated + bufsz_inc
          ); if (!new_buf) return "Could not enlarge a growing buffer!";
          *(void **)v->buffer_ref= buffer= new_buf;
          *v->size_ref= allocated= new_size;
+         if (bufsz_inc < max_bufsz_inc) {
+            if ((bufsz_inc+= bufsz_inc) > max_bufsz_inc) {
+               bufsz_inc= max_bufsz_inc;
+            }
+         }
       }
       assert(allocated > used);
       if (
--- include/append_rest_of_file-6p7cqka4jp2g26pkn2f0pyleu.h
+++ include/append_rest_of_file-6p7cqka4jp2g26pkn2f0pyleu.h
@@ -9,9 +9,9 @@ extern "C" {
 
 struct append_rest_of_file_vars {
    FILE *fh; /* An open file. */
-   /* assert((*(void **)buffer_ref == NULL) == (*size_ref == 0)); */
-   void *buffer_ref; /* Address of buffer pointer variable. */
-   size_t *size_ref; /* Address of buffer size variable. */
+   /* *(void **)buffer_ref must be null if *size_ref == 0. */
+   void *buffer_ref; /* A void **: Address of buffer pointer variable. */
+   size_t *size_ref; /* Address of variable for actually used buffer bytes. */
 };
 
 /* Append the unread rest of an open file to a buffer, enlarging it as
